b18bd144e6815b151d37e862265e0c309f39aff4
Taro <nathanon.chi@student.mahidol.edu>
2025-10-25 16:50:35 +0700

    Set up the assignment tracker

---
+++ WebApp/src/main/java/dev/taro/webapp/Main.java
@@ -0,0 +1,8 @@
+package dev.taro.webapp;
+import io.muzoo.ssc.assignment.tracker.SscAssignment;
+public class Main extends SscAssignment {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+    }
+}
